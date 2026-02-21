6d5cf0a30b34b43194404508df7fc72ca533c44f
Hafsa Imran <hafsaemran1712@gmail.com>
2026-02-21 12:54:44 +0500

    Assignment Done

---
+++ Chapter 1/app.js
@@ -0,0 +1,15 @@
+// Question 1
+// alert("hello");
+
+// Question 2
+// alert("Error! Please enter a valid pasword.");
+
+// Question 3
+// alert("Welcome to js land... \n Happy Coding!");
+
+// Question 4
+// alert("Welcome to js land...");
+// alert("Happy Coding!");
+
+// Question 5
+// alert("Hello... Can I run JS througn my web browser's console");
