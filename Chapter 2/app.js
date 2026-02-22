9ebddcaedc1ef970ade54eee90fb0eb27c3ec58f
Hafsa Imran <hafsaemran1712@gmail.com>
2026-02-22 11:24:39 +0500

    Assignment Complete

---
+++ Chapter 2/app.js
@@ -0,0 +1,39 @@
+// question 1
+var username;
+
+// question 2
+var myName = "Hafsa Imran";
+
+// question 3
+var title = "Hello World";
+alert(title);
+
+// question 4
+var studentName = "Jhone Doe";
+var studentAge = "15 years old";
+var studentCourse = "Certified Mobile Application Development";
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+// question 5
+var item = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(item);
+
+// question 6
+var email = "My email is " + "example@example.com";
+alert(email);
+
+// question 7
+var book = "A smarter way to learn JavaScript";
+alert(book);
+
+// question 8
+var text = "Yeah! I can write HTML content through JavaScript"
+document.write(text);
+document.write("<br \>");
+
+// question 9
+let message = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(message);
+document.write(message);
