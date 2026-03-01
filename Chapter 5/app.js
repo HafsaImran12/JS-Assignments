3d1a7e884d01cc5e86b370514244ff4235a19e71
Hafsa Imran <hafsaemran1712@gmail.com>
2026-03-01 07:22:36 +0500

    Assignment Done

---
+++ Chapter 5/app.js
@@ -0,0 +1,49 @@
+// Question 01
+// let a=3, b=5;
+// let c=a+b;
+// document.write("Sum of 3 and 5 is " +c +"<br/>");
+
+// Question 02
+// let d=a-b;
+// document.write("subtraction of 3 and 5 is " +d +"<br/>");
+// let e=a*b;
+// document.write("Multiplication of 3 and 5 is " +e +"<br/>");
+// let f=a/b;
+// document.write("Division of 3 and 5 is " +f +"<br/>");
+// let g=a%b;
+// document.write("Modulus of 3 and 5 is " +g +"<br/>");
+
+// Question 02
+// a)
+let number;
+// b)
+document.write("Value after variable declaration is: undefined <br/>");
+// c)
+number = 5;
+// d)
+document.write("Initial value: " + number +"<br/>");
+// e)
+number++;
+// f)
+document.write("Value after increment is: " + number +"<br/>");
+// g)
+number=number+7;
+// h)
+document.write("Value after addition is: " + number +"<br/>");
+// i)
+number--;
+// j)
+document.write("Value after decrement is: " + number +"<br/>");
+// k)
+number=number%3;
+document.write("The remainder is : " + number +"<br/>");
+// l)
+let ticketPrice=600;
+document.write("Cost of one movie ticket is " + ticketPrice + " PKR. <br/>");
+ticketPrice=ticketPrice*5;
+document.write("Total cost to buy 5 tickets to a movie is " + ticketPrice +"<br/>");
+// l)
+let num=4;
+for(let i=1; i <=10; i++){
+    document.write(num + "x" + i + "=" + (num*i) +"<br/>");
+}
