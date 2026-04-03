918e5a80685e4439776af1db188f30b1b939be79
Hafsa Imran <hafsaemran1712@gmail.com>
2026-04-03 17:57:47 +0500

    Assignment Done

--- Chapter 12-13/app.js
+++ Chapter 12-13/app.js
@@ -14,14 +14,23 @@
 // }
 
 // Question 01
-let a = +prompt("Enter first number");
-let b = +prompt("Enter second number");
-if (a > b) {
-  alert(`${a} is greater than ${b}`);
-} else if (a < b) {
-  alert(`${a} is less than ${b}`);
-} else if (a == b) {
-  alert(`${a} is equal ${b}`);
+// let a = +prompt("Enter first number");
+// let b = +prompt("Enter second number");
+// if (a > b) {
+//   alert(`${a} is greater than ${b}`);
+// } else if (a < b) {
+//   alert(`${a} is less than ${b}`);
+// } else if (a == b) {
+//   alert(`${a} is equal ${b}`);
+// } else {
+//   alert("Special character");
+// }
+
+let num = +prompt("Enter Number")
+if (num > 0) {
+    alert("Positive Number")
+} else if (num < 0) {
+    alert("Negative Number")
 } else {
-  alert("Special character");
+    alert("Zero")
 }
