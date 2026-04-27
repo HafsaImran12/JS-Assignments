20eddb92af2e8bfd862ecf70c739048fc0cb8304
Hafsa Imran <hafsaemran1712@gmail.com>
2026-04-27 10:56:24 +0500

    Assignment Done

--- Chapter 21-25/app.js
+++ Chapter 21-25/app.js
@@ -1,17 +1,88 @@
 // Question no 01
 
-let firstName = prompt("Enter your first name:");
-let lastName = prompt("Enter your last name:");
-let fullName = firstName + lastName;
-alert(`Hello ${fullName}`);
+// let firstName = prompt("Enter your first name:");
+// let lastName = prompt("Enter your last name:");
+// let fullName = firstName + lastName;
+// alert(`Hello ${fullName}`);
 
 // Question no 02
 
+// let userInput = prompt("Enter your favourite mobile model");
+// document.write(`My favourite phone is: ${userInput}`);
+// document.write("<br/>");
+// document.write(`Length of string is: ${userInput.length}`)
 
+// Question no 03
 
+// let str = "Pakistani";
+// let newStr =str.indexOf("n");
+// document.write(`String ${str}`);
+// document.write("<br/>");
+// document.write(`Index of 'n' is ${newStr}`);
 
+// Question no 04
 
-// Question no 01
-// Question no 01
-// Question no 01
-// Question no 01
+// let str = "Hello World";
+// let newStr =str.lastIndexOf("l");
+// document.write(`String ${str}`);
+// document.write("<br/>");
+// document.write(`Last index of 'l' is ${newStr}`);
+
+// Question no 05
+
+// let str = "Pakistani";
+// let newStr =str.charAt(3);
+// document.write(`String ${str}`);
+// document.write("<br/>");
+// document.write(`Character at index 3 is ${newStr}`);
+
+// Question no 06
+
+// let firstName = prompt("Enter your first name:");
+// let lastName = prompt("Enter your last name:");
+// let fullName = firstName.concat(lastName);
+// alert(`Hello ${fullName}`);
+
+// Question no 07
+
+// let str = "Hyderabad"
+// let str2 = str.replace("Hyderabad", "Islamabad");
+// document.write(str)
+// document.write("<br/>");
+// document.write(str2)
+
+// Question no 08
+
+// let message = "Ali and Sami are best friends. They play cricket and football together.";
+// let newMessage = message.replace(/and/g, "&");
+// document.write(message);
+// document.write("<br/>");
+// document.write(newMessage);
+
+// Question no 09
+
+// let str = "472";
+// let newStr = Number(str);
+// document.write(`Value: ${newStr}`);
+// document.write("<br/>");
+// document.write(`Type: ${typeof newStr}`);
+
+// Question no 10
+
+// let str = "472";
+// let newStr = Number(str);
+// document.write(`Value: ${str}`);
+// document.write("<br/>");
+// document.write(`Type: ${typeof str}`);
+// document.write("<br/>");
+// document.write(`Value: ${newStr}`);
+// document.write("<br/>");
+// document.write(`Type: ${typeof newStr}`);
+
+// Question no 11
+
+let userInput = prompt("Enter a fruit");
+let upperCase = userInput.toUpperCase();
+document.write(`User input: ${userInput}`);
+document.write("<br/>");
+document.write(`Upper case: ${upperCase}`);
