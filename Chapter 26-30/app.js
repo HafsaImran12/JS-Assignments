b30f59ecb1a580a4cda04d74c2d2a8c9547141cb
Hafsa Imran <hafsaemran1712@gmail.com>
2026-04-30 11:58:45 +0500

    Assignment Complete

---
+++ Chapter 26-30/app.js
@@ -0,0 +1,23 @@
+// let number = +prompt("Enter a floating point number");
+// document.write(`number: ${number} <br/>`);
+
+// let roundNumber = Math.round(number);
+// document.write(`round number: ${roundNumber} <br/>`);
+
+// let floorNumber = Math.floor(number);
+// document.write(`floor number: ${floorNumber} <br/>`);
+
+// let cielNumber = Math.ceil(number);
+// document.write(`ciel number: ${cielNumber}`);
+
+// let negNumber = +prompt("Enter a negative floating point number");
+// document.write(`number: ${negNumber} <br/>`);
+
+// let roundNumber = Math.round(negNumber);
+// document.write(`round number: ${roundNumber} <br/>`);
+
+// let floorNumber = Math.floor(negNumber);
+// document.write(`floor number: ${floorNumber} <br/>`);
+
+// let cielNumber = Math.ceil(negNumber);
+// document.write(`ciel number: ${cielNumber}`);
