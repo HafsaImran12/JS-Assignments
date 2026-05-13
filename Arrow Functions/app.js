59a225f2b0e910115a51021d4a12d87890785fdb
Hafsa Imran <hafsaemran1712@gmail.com>
2026-05-13 17:17:09 +0500

    Assignment Complete

---
+++ Arrow Functions/app.js
@@ -0,0 +1,81 @@
+// 01
+// let userNumber1= +prompt("Enter first number")
+// let userNumber2= +prompt("Enter second number")
+// let sum = (num1, num2) => num1 + num2;
+// console.log(sum(userNumber1,userNumber2));
+
+// 02
+
+// let userNumber = +prompt("Enter a number")
+// let checkNumber = (num) => {
+//     if(userNumber % 2 == 0) {
+//         alert(`${num} is even number`);
+//     } else {
+//         alert(`${num} is odd number`)
+//     }
+// }
+// checkNumber(userNumber)
+
+// 03
+
+// let userNumber1 = +prompt("Enter first number");
+// let userNumber2 = +prompt("Enter second number");
+// let greater = (num1, num2) => {
+//   if (num1 > num2) {
+//     alert(`${num1} is greater than ${num2}`);
+//   } else if (num1 < num2) {
+//     alert(`${num1} is less than ${num2}`);
+//   } else {
+//     alert("Not a Number");
+//   }
+// };
+// greater(userNumber1, userNumber2);
+
+// 04
+
+// let userMarks = +prompt("Enter your marks")
+// let marks = () => {
+//     if (userMarks > 40) {
+//         alert(`Congratulations! You will pass`)
+//     } else {
+//         alert("Fail")
+//     }
+// }
+// marks(userMarks)
+
+// 05
+
+// let userNumber = +prompt("Enter a number")
+// let checks = () =>{
+//     if (userNumber > 0) {
+//         alert("Positive Number")
+//     } else if (userNumber < 0) {
+//         alert("Negative Number")
+//     } else {
+//         alert("Zero")
+//     }
+// }
+// checks(userNumber)
+
+// 06
+
+// let square = (num) => num * num;
+// console.log(square(3));
+
+// 07
+
+// let str = (upper) => upper.toUpperCase()
+// console.log(str("hello"));
+
+// 08
+
+// let str = (lower) => lower.toLowerCase()
+// console.log(str("HELLO"));
+
+// 09
+
+let str = (empty) => empty
+console.log(str(""));
+
+
+
