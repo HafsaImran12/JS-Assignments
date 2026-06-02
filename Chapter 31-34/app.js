cc67b1560239c4889e3eabd070bb8a0bc499a6b5
Hafsa Imran <hafsaemran1712@gmail.com>
2026-06-02 16:36:59 +0500

    Assignment Complete

--- Chapter 31-34/app.js
+++ Chapter 31-34/app.js
@@ -1,25 +1,77 @@
 // ASSIGNMENT 31-34
 
-// 01.
+// // 01.
 
 // let now = new Date()
-// document.write(now);
+// document.write(`${now} <br/>`);
 
-// 02.
+// // 02.
 
 // let months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
 // let month = new Date();
 // let currentMonth = month.getMonth();
-// console.log(months[currentMonth]);
+// document.write(`Current month: ${months[currentMonth]} <br/>`);
 
-// 03.
+// // 03.
 
 // let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 // let day = new Date();
 // let currentDay = day.getDay();
-// console.log(days[currentDay].slice(0,3));
+// document.write(`Today is ${days[currentDay].slice(0,3)}`);
 
-// 04.
+// // 04.
 
+// let day = new Date()
+// let currentDay = day.getDay();
 
+// if (currentDay == 0) {
+//     document.write("It's fun day")
+// } else if (currentDay == 1) {
+//     document.write("Monday")
+// } else if (currentDay == 2) {
+//     document.write("Tuesday")
+// } else if (currentDay == 3) {
+//     document.write("Wednesday")
+// } else if (currentDay == 4) {
+//     document.write("Thursday")
+// } else if (currentDay == 5) {
+//     document.write("Friday")
+// } else if (currentDay == 6) {
+//     document.write("It's fun day")
+// }
+
+// // 05.
+
+// let day = new Date()
+// let currentDay = day.getDate();
+// if (currentDay < 15) {
+//     document.write(`First fifteen days of the month`)
+// } else {
+//     document.write(`last days of the month`)
+// }
+
+// // 06.
+
+// let now = new Date();
+// document.write(`Current Date: ${now} <br/> <br/>`);
+// let currentTime = now.getTime();
+// document.write(`Elapsed milliseconds since January 1, 1970: ${currentTime} <br/> <br/>`);
+// let elapsedMinutes = Math.floor(currentTime / (1000 * 60));
+// document.write(`Elapsed minutes since January 1, 1970: ${elapsedMinutes}`);
+
+// // 07.
+
+// let now = new Date();
+// let hour = now.getHours();
+// if (hour < 12) {
+//     document.write("Its AM")
+// } else {
+//     document.write("Its PM")
+// }
+
+// // 08.
+
+let now = new Date();
+
+let latter = 
 
